c31da961fbf0c016d97f289a31b75cdf4ff7f554
Urik Dmitro <urikdmitro@gmail.com>
2024-08-13 15:05:13 +0300

    (chore)

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -5,8 +5,9 @@ SCRIPT_DIRECTORY="$(dirname "${SCRIPT_PATH}")"
 
 cd ${SCRIPT_DIRECTORY}/..
 
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -B build .
-cmake --build build
+PREFIX_PATH="/home/dmitro/Documents/Projects/C/NBodyProject/Nbody/vendor/OpenCL-Headers/install;/home/dmitro/Documents/Projects/C/NBodyProject/Nbody/vendor/OpenCL-ICD-Loader/install;/home/dmitro/Documents/Projects/C/NBodyProject/Nbody/vendor/OpenCL-CLHPP/install"
 
+cmake -D CMAKE_PREFIX_PATH="${PREFIX_PATH}" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -B build .
+cmake --build build
 
 build/N_Body
